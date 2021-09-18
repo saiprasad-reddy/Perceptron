e118815a410a24af9a08f0fe5bab5461ae6b11c8
Saiprasad Reddy <saiprasadreddybussa@gmail.com>
2021-09-18 15:09:35 +0530

    code organised

--- AND.py
+++ AND.py
@@ -3,23 +3,25 @@ from utils.all_utils import prepare_data, save_plot, save_model
 import pandas as pd
 import numpy as np
 
+def main(data, eta, epochs, filename, plotfinename):
+        
+        df = pd.DataFrame(data)
+        df
 
-AND = {
+        X,y = prepare_data(df)
+        model = Perceptron(eta=eta, epochs=epochs)
+        model.fit(X, y)
+        loss = model.total_loss
+
+        save_model(model, filename=filename)
+        save_plot(df, plotfinename, model)
+if __name__ == '__main__':
+        
+        AND = {
         "x1":[0,0,1,1],
         "x2":[0,1,0,1],
         "y":[0,0,0,1]
-}
-df = pd.DataFrame(AND)
-df
-
-
-
-X,y = prepare_data(df)
-eta = 0.3 #learning rate should be between 0 and 1
-epochs = 10
-model_AND = Perceptron(eta=eta, epochs=epochs)
-model_AND.fit(X, y)
-loss = model_AND.total_loss
-
-save_model(model_AND, filename="AND.model")
-save_plot(df, "AND.png", model_AND)
+        }
+        eta = 0.3 #learning rate should be between 0 and 1
+        epochs = 10
+        main(data=AND,ETA=eta,EPOCHS=epochs,filename="AND.model", plotfinename="AND.png")
---
+++ OR.py
@@ -0,0 +1,27 @@
+from utils.model import Perceptron
+from utils.all_utils import prepare_data, save_plot, save_model
+import pandas as pd
+import numpy as np
+
+def main(data, eta, epochs, filename, plotfinename):
+        
+        df = pd.DataFrame(data)
+        df
+
+        X,y = prepare_data(df)
+        model = Perceptron(eta=eta, epochs=epochs)
+        model.fit(X, y)
+        loss = model.total_loss
+
+        save_model(model, filename=filename)
+        save_plot(df, plotfinename, model)
+if __name__ == '__main__':
+        
+        OR = {
+        "x1":[0,0,1,1],
+        "x2":[0,1,0,1],
+        "y":[1,1,1,0]
+        }
+        eta = 0.3 #learning rate should be between 0 and 1
+        epochs = 10
+        main(data=OR, eta=eta, epochs=epochs, filename="OR.model", plotfinename="OR.png")
